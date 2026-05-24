b8ea2642cbd6ba32a611a488d14d7747e16802b3
Shivampahuja12 <shivampahuja1212@gmail.com>
2026-05-24 19:04:43 +0530

    Connected PostgreSQL

--- server/index.js
+++ server/index.js
@@ -66,3 +66,17 @@ server.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
     connectDb()
 })
+
+
+//temporary route to test database connection
+const pool = require('./config/db');
+
+app.get('/test-db', async (req, res) => {
+  try {
+    const result = await pool.query('SELECT NOW()');
+    res.json(result.rows);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: err.message });
+  }
+});
